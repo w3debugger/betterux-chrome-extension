9f14c29afed1e95de3633c039563996630205b34
Umar <w3debugger@gmail.com>
2020-12-13 20:44:55 +0400

    Fix domain name with WEB

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
 chrome.browserAction.onClicked.addListener(async ({ id, url }) => {
   const { hostname } = url ? new URL(url) : {};
   if (!hostname) { return; }
+  
+  const hostnameArray = hostname.replace(/^www.|^en./g, '').split('.');
+  const enrichHostname = hostnameArray.length > 1 ? hostnameArray.splice(0, hostnameArray.length - 1).join('') : hostnameArray;
 
-  const [enrichHostname] = hostname.replace(/^www.|^en./g, '').split('.');
   const data = await fetch('https://betterux.io/static/json/sites.json')
     .then(res => res.json())
     .then(res => res.find(({ url }) => url === enrichHostname))
